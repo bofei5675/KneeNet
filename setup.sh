8ff28001721ce0e2371ff1c850ed07cb87bbfa65
Zhang <bz1030@skynet01.skynet.cluster>
2019-03-25 16:04:17 -0400

    more info

---
+++ setup.sh
@@ -0,0 +1,7 @@
+## pytorch environment
+export PATH=/gpfs/share/skynet/apps/anaconda3/bin:$PATH
+. /gpfs/share/skynet/apps/anaconda3/etc/profile.d/conda.sh
+source activate pytorch-env
+source /opt/DL/pytorch/bin/pytorch-activate
+
+source activate knee_localizer
